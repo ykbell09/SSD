fb8c64ada94d4c2701b58002f7396a0556c69498
Yvette Bell <ykbell09@gmail.com>
2020-03-21 12:24:03 -0700

    update migration

--- migrations/20200321121311_allTables.js
+++ migrations/20200321121311_allTables.js
@@ -5,7 +5,6 @@ exports.up = async knex => {
         table.increments('id').primary();
         table.string('distiller_name').unique().notNullable();
         table.timestamp('added_on').defaultTo(knex.fn.now()).notNullable();
-
     });
     await knex.schema.dropTableIfExists('spirits');
     await knex.schema.createTable('spirits', table => {
@@ -17,14 +16,14 @@ exports.up = async knex => {
             .references('distiller_id')
             .onDelete('cascade')
             .onUpdate('cascade');
-    })
+    });
     await knex.schema.dropTableIfExists('members');
     await knex.schema.createTable('members', table => {
         table.increments('id').primary();
         table.string('email_address', 30).notNullable().unique();
         table.string('password').notNullable().unique();
         table.timestamp('joined').defaultTo(knex.fn.now()).notNullable();
-    })
+    });
 };
 
 exports.down = function (knex) {
