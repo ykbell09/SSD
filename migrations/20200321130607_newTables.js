a8730dcc1787c2880a9a4cda6100c7ccb6c0a232
Yvette Bell <ykbell09@gmail.com>
2020-03-21 14:08:29 -0700

    update migrations

---
+++ migrations/20200321130607_newTables.js
@@ -0,0 +1,30 @@
+
+exports.up = async knex => {
+
+    await knex.schema.dropTableIfExists('distillers');
+    await knex.schema.createTable('distillers', table => {
+        table.increments('id').primary();
+        table.string('distiller_name').unique().notNullable();
+        table.timestamp('added_on').defaultTo(knex.fn.now()).notNullable();
+    });
+
+    await knex.schema.dropTableIfExists('spirits');
+    await knex.schema.createTable('spirits', table => {
+        table.increments('id').primary();
+        table.string('spirit_name').notNullable();
+        table.integer('distiller_id');
+        table.unique(['spirit_name', 'distiller_id']);
+        table.foreign('distiller_id')
+            .references('id')
+            .inTable('distillers')
+            .onDelete('cascade')
+            .onUpdate('cascade');
+    });
+
+};
+
+exports.down = async knex => {
+    await knex.schema.dropTableIfExists('distillers');
+    await knex.schema.dropTableIfExists('spirits');
+
+};
