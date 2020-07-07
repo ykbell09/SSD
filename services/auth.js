33a7a3600f4fb9bbe72b8cb858930663aacf8529
Yvette Bell <ykbell09@gmail.com>
2020-07-07 10:46:08 -0700

    misc updates

--- services/auth.js
+++ services/auth.js
@@ -3,12 +3,6 @@ import bcrypt from 'bcrypt';
 const saltRounds = 10;
 export const hashPass = async rawPass => 
     await bcrypt.hash(rawPass, saltRounds);
-;
 
 export const compareHash = async (rawPass, hashedPass) =>
     await bcrypt.compare(rawPass, hashedPass);
-
-// export const validatePassword = candidate => {
-//     if (candidate.length < 6)
-//         throw new Error('please enter at least 6 characters');
-// };
